4becd6fee4efbc1e63329e3b9a185da48e0322bb
proaymen941-netizen <proaymen941@gmail.com>
2025-09-23 04:04:52 +0300

    Add files via upload

--- server/vite(1).ts
+++ server/vite(1).ts
@@ -1,9 +1,11 @@
-// server/viteServer.ts
+// server/vite.ts
 import * as vite from "vite";
+
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
 import { type Server } from "http";
+import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
 import { fileURLToPath } from "url";
 
@@ -28,6 +30,7 @@ export async function setupVite(app: Express, server: Server) {
 
   // إنشاء Vite server
   const viteServer = await vite.createServer({
+    ...viteConfig,
     configFile: path.resolve(__dirname, "..", "client", "vite.config.ts"),
     server: serverOptions,
     appType: "custom",
